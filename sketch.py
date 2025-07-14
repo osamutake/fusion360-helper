edc4ca573a973a8a833c6658473c12d87837c1cf
Osamu Takeuchi <osamu@big.jp>
2025-07-14 14:07:50 +0900

    add sketch_fillet, improve sketch_rectangle

--- sketch.py
+++ sketch.py
@@ -36,10 +36,17 @@ def sketch_rectangle(
     sketch: adsk.fusion.Sketch,
     corner1: Vector | adsk.core.Point3D | adsk.fusion.SketchPoint,
     corner2: Vector | adsk.core.Point3D | adsk.fusion.SketchPoint,
-    origin: adsk.fusion.SketchPoint | None = None,
+    reference: adsk.fusion.SketchPoint | None = None,
     fillet: float | None = None,
+    square: bool = False,
 ):
-    """Add a rectangle defined by two corner points."""
+    """Add a rectangle defined by two corner points.
+    Optionally, an reference point can be specified to constrain the rectangle's position.
+    If a fillet radius is provided, fillets will be added to the corners.
+    Returns a list of sketch curves created, including lines in the order
+    of bottom, right, top and left, and fillets, if applicable, in the order
+    of lower-left, lower-right, upper-right, and upper-lef.
+    """
 
     p1 = corner1.geometry if isinstance(corner1, adsk.fusion.SketchPoint) else corner1
     p2 = corner2.geometry if isinstance(corner2, adsk.fusion.SketchPoint) else corner2
@@ -60,69 +67,131 @@ def sketch_rectangle(
         ),
         point3d((p1.x + p2.x) / 2, p1.y - 0.2),
     )
-    sketch.sketchDimensions.addDistanceDimension(
-        l4.startSketchPoint,
-        l4.endSketchPoint,
-        cast(
-            adsk.fusion.DimensionOrientations,
-            adsk.fusion.DimensionOrientations.VerticalDimensionOrientation,
-        ),
-        point3d(p1.x - 0.2, (p1.y + p2.y) / 2),
-    )
-    if origin is not None:
-        if origin.geometry.x == p1.x and origin.geometry.y == p1.y:
-            # sketch.geometricConstraints.addCoincident(l1.startSketchPoint, origin)
-            pass
-        else:
-            if origin.geometry.x == p1.x:
+    if not square:
+        sketch.sketchDimensions.addDistanceDimension(
+            l4.startSketchPoint,
+            l4.endSketchPoint,
+            cast(
+                adsk.fusion.DimensionOrientations,
+                adsk.fusion.DimensionOrientations.VerticalDimensionOrientation,
+            ),
+            point3d(p1.x - 0.2, (p1.y + p2.y) / 2),
+        )
+
+    if fillet is not None and fillet > 0:
+        f1 = sketch_fillet(sketch, l1, l4, fillet)
+        f2 = sketch_fillet(sketch, l2, l1, fillet)
+        f3 = sketch_fillet(sketch, l3, l2, fillet)
+        f4 = sketch_fillet(sketch, l4, l3, fillet)
+        sketch.sketchDimensions.addRadialDimension(
+            f1, point3d(-2 * fillet, -2 * fillet, 0)
+        )
+        sketch.geometricConstraints.addEqual(f1, f2)
+        sketch.geometricConstraints.addEqual(f2, f3)
+        sketch.geometricConstraints.addEqual(f3, f4)
+
+    if square:
+        sketch.geometricConstraints.addEqual(l1, l4)
+
+    if reference is not None:
+        if (
+            abs(reference.geometry.x - p1.x) < 0.0001
+            and abs(reference.geometry.y - p1.y) < 0.0001
+            and (fillet is None or fillet == 0)
+        ):
+            try:
                 sketch.geometricConstraints.addCoincident(
-                    origin,
-                    l4,
+                    l1.startSketchPoint, reference
                 )
+            except RuntimeError:
+                # If the coincident constraint fails, we skip it
+                pass
+        else:
+            if abs(reference.geometry.x - p1.x) < 0.0001:
+                try:
+                    sketch.geometricConstraints.addCoincident(
+                        reference,
+                        l4,
+                    )
+                except RuntimeError:
+                    # If the coincident constraint fails, we skip it
+                    pass
             else:
                 sketch.sketchDimensions.addDistanceDimension(
-                    origin,
-                    l1.startSketchPoint,
+                    reference,
+                    l4.endSketchPoint,
                     cast(
                         adsk.fusion.DimensionOrientations,
                         adsk.fusion.DimensionOrientations.HorizontalDimensionOrientation,
                     ),
-                    point3d((origin.geometry.x + p1.x) / 2, origin.geometry.y - 0.2),
+                    point3d(
+                        (reference.geometry.x + p1.x) / 2, reference.geometry.y - 0.2
+                    ),
                 )
-            if origin.geometry.y == p1.y:
-                sketch.geometricConstraints.addCoincident(
-                    origin,
-                    l1,
-                )
+            if abs(reference.geometry.y - p1.y) < 0.0001:
+                try:
+                    sketch.geometricConstraints.addCoincident(
+                        reference,
+                        l1,
+                    )
+                except RuntimeError:
+                    # If the coincident constraint fails, we skip it
+                    pass
             else:
                 sketch.sketchDimensions.addDistanceDimension(
-                    origin,
+                    reference,
                     l1.startSketchPoint,
                     cast(
                         adsk.fusion.DimensionOrientations,
                         adsk.fusion.DimensionOrientations.VerticalDimensionOrientation,
                     ),
-                    point3d(origin.geometry.x - 0.2, (origin.geometry.y + p1.y) / 2),
+                    point3d(
+                        reference.geometry.x - 0.2, (reference.geometry.y + p1.y) / 2
+                    ),
                 )
-    if fillet is None:
+
+    if fillet is None or fillet <= 0:
         return [l1, l2, l3, l4]
-    f1 = sketch.sketchCurves.sketchArcs.addFillet(
-        l1, l1.startSketchPoint.geometry, l4, l4.endSketchPoint.geometry, fillet
-    )
-    f2 = sketch.sketchCurves.sketchArcs.addFillet(
-        l2, l2.startSketchPoint.geometry, l1, l1.endSketchPoint.geometry, fillet
-    )
-    f3 = sketch.sketchCurves.sketchArcs.addFillet(
-        l3, l3.startSketchPoint.geometry, l2, l2.endSketchPoint.geometry, fillet
-    )
-    f4 = sketch.sketchCurves.sketchArcs.addFillet(
-        l4, l4.startSketchPoint.geometry, l3, l3.endSketchPoint.geometry, fillet
-    )
-    sketch.sketchDimensions.addRadialDimension(f1, point3d(-2 * fillet, -2 * fillet, 0))
-    sketch.geometricConstraints.addEqual(f1, f2)
-    sketch.geometricConstraints.addEqual(f2, f3)
-    sketch.geometricConstraints.addEqual(f3, f4)
     return [l1, l2, l3, l4, f1, f2, f3, f4]
+
+
+def sketch_fillet(
+    sketch: adsk.fusion.Sketch,
+    line1: (
+        adsk.fusion.SketchLine
+        | adsk.fusion.SketchArc
+        | adsk.fusion.SketchFittedSpline
+        | adsk.fusion.SketchControlPointSpline
+    ),
+    line2: (
+        adsk.fusion.SketchLine
+        | adsk.fusion.SketchArc
+        | adsk.fusion.SketchFittedSpline
+        | adsk.fusion.SketchControlPointSpline
+    ),
+    radius: float,
+    add_dimension: bool = False,
+):
+    """Add a fillet between two lines at specified points."""
+    point1 = line1.startSketchPoint.geometry
+    if point1.isEqualToByTolerance(line2.startSketchPoint.geometry, 0.0001):
+        point2 = line2.startSketchPoint.geometry
+    elif point1.isEqualToByTolerance(line2.endSketchPoint.geometry, 0.0001):
+        point2 = line2.endSketchPoint.geometry
+    else:
+        point1 = line1.endSketchPoint.geometry
+        if point1.isEqualToByTolerance(line2.startSketchPoint.geometry, 0.0001):
+            point2 = line2.startSketchPoint.geometry
+        elif point1.isEqualToByTolerance(line2.endSketchPoint.geometry, 0.0001):
+            point2 = line2.endSketchPoint.geometry
+        else:
+            raise ValueError("The lines do not share a common point for fillet.")
+
+    f = sketch.sketchCurves.sketchArcs.addFillet(line1, point1, line2, point2, radius)
+
+    if add_dimension:
+        sketch.sketchDimensions.addRadialDimension(f, point1)
+    return f
 
 
 def sketch_fitted_splines(
